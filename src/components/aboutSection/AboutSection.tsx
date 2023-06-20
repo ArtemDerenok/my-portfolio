e2f8e93e4553577b4ebf90e71c54cf47dae1acaa
ArtemDerenok <derenokartem95@gmail.com>
2023-06-20 14:10:44 +0300

    feat: add base layout for about component

--- src/components/aboutSection/AboutSection.tsx
+++ src/components/aboutSection/AboutSection.tsx
@@ -1,5 +1,31 @@
+import styles from "./AboutSection.module.scss";
+
 const AboutSection = () => {
-  return <div></div>;
+  return (
+    <div className={styles.about}>
+      <h1>
+        Hello I’m
+        <br /> Artyom Derenok
+      </h1>
+      <p>
+        I am a UI Engineer at Google, where I design and develop user-centered
+        interfaces for cutting-edge technology products. I work with a talented
+        team to create visually appealing and intuitive designs that enhance
+        user experience. I am passionate about using technology to create
+        beautiful and functional designs that solve real-world problems.
+      </p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>React</li>
+        <li>Webpack</li>
+        <li>NestJS</li>
+        <li>NodeJS</li>
+      </ul>
+    </div>
+  );
 };
 
 export default AboutSection;
