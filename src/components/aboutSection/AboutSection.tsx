911caed4a814bf27ec85db6873adbc0f0f73636c
ArtemDerenok <derenokartem95@gmail.com>
2023-06-21 21:19:45 +0300

    feat: add table and laptop layout for firstsection

--- src/components/aboutSection/AboutSection.tsx
+++ src/components/aboutSection/AboutSection.tsx
@@ -1,4 +1,53 @@
+import htmlIcon from "../../assets/icons/html.svg";
+import cssIcon from "../../assets/icons/css.svg";
+import jsIcon from "../../assets/icons/js.svg";
+import tsIcon from "../../assets/icons/ts.svg";
+import reactIcon from "../../assets/icons/react.svg";
+import webpackIcon from "../../assets/icons/webpack.png";
+import nestIcon from "../../assets/icons/node.svg";
+import nodeIcon from "../../assets/icons/node.svg";
 import styles from "./AboutSection.module.scss";
+import Tag from "../tag/Tag";
+
+interface ITagObj {
+  text: string;
+  icon: string;
+}
+
+const iconsArr: ITagObj[] = [
+  {
+    text: "HTML",
+    icon: htmlIcon,
+  },
+  {
+    text: "CSS",
+    icon: cssIcon,
+  },
+  {
+    text: "JavaScript",
+    icon: jsIcon,
+  },
+  {
+    text: "TypeScript",
+    icon: tsIcon,
+  },
+  {
+    text: "React",
+    icon: reactIcon,
+  },
+  {
+    text: "Webpack",
+    icon: webpackIcon,
+  },
+  {
+    text: "NestJS",
+    icon: nestIcon,
+  },
+  {
+    text: "NodeJS",
+    icon: nodeIcon,
+  },
+];
 
 const AboutSection = () => {
   return (
@@ -7,7 +56,7 @@ const AboutSection = () => {
         Hello I’m
         <br /> Artyom Derenok
       </h1>
-      <p>
+      <p className={styles.about__text}>
         I am a UI Engineer at Google, where I design and develop user-centered
         interfaces for cutting-edge technology products. I work with a talented
         team to create visually appealing and intuitive designs that enhance
@@ -15,14 +64,9 @@ const AboutSection = () => {
         beautiful and functional designs that solve real-world problems.
       </p>
       <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>React</li>
-        <li>Webpack</li>
-        <li>NestJS</li>
-        <li>NodeJS</li>
+        {iconsArr.map((elem, index) => (
+          <Tag text={elem.text} icon={elem.icon} key={index} />
+        ))}
       </ul>
     </div>
   );
--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,5 +1,17 @@
-const Tag = () => {
-  return <div></div>;
+import styles from "./Tag.module.scss";
+
+interface ITag {
+  text: string;
+  icon: string;
+}
+
+const Tag = ({ text, icon }: ITag) => {
+  return (
+    <li className={styles.tag}>
+      <img src={icon} alt={text} />
+      <p className={styles.tag__text}>{text}</p>
+    </li>
+  );
 };
 
 export default Tag;
