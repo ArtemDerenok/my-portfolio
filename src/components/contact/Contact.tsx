f37484711de2066edf4134d049017ecffda040da
ArtemDerenok <derenokartem95@gmail.com>
2023-06-27 20:53:49 +0300

    feat: add form validation

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,24 @@
 import contactImg from "../../assets/icons/contact.png";
+import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./Contact.module.scss";
 
+interface IFormInput {
+  name: string;
+  email: string;
+  message: string;
+}
+
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
   return (
     <section className={`${styles.container} ${styles.contact}`}>
       <div className={styles.contact__boxOne}>
@@ -14,17 +31,40 @@ const Contact = () => {
         <img src={contactImg} alt="contacts" className={styles.contact__img} />
       </div>
       <div className={styles.contact__boxTwo}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" className={styles.form__name} />
+          <input
+            type="text"
+            id="name"
+            className={styles.form__name}
+            {...(register("name"), { required: true, minLength: 2 })}
+          />
+          {errors.message?.type === "required" ? (
+            <p className={styles.error}>Name is required</p>
+          ) : errors.message?.type === "minLength" ? (
+            <p className={styles.error}>Name is too short</p>
+          ) : null}
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" className={styles.form__email} />
+          <input
+            type="email"
+            id="email"
+            className={styles.form__email}
+            {...(register("email"), { required: true })}
+          />
+          {errors.email?.type === "required" ? (
+            <p className={styles.error}>Email is required</p>
+          ) : null}
           <label htmlFor="message">Message</label>
           <textarea
-            name=""
             id="message"
             className={styles.form__message}
+            {...register("message", { required: true, minLength: 20 })}
           ></textarea>
+          {errors.message?.type === "required" ? (
+            <p className={styles.error}>Message is required</p>
+          ) : errors.message?.type === "minLength" ? (
+            <p className={styles.error}>Message is too short</p>
+          ) : null}
           <button className={styles.form__button}>Submit</button>
         </form>
       </div>
